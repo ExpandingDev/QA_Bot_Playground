acbb03be0afca363729e1e6acd2bc93bd09255b2
Tyler Sengia <tylersengia@gmail.com>
2022-01-02 04:51:59 +0000

    Adding statement and question models

--- models/models.py
+++ models/models.py
@@ -1,2 +1,19 @@
 
 
+class Statement(db.Model):
+    __tablename__ = "StatementList"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    text = db.Column(db.String(300), nullable=False)
+
+    entry_time = db.Column(db.DateTime(), nullable=False, default=datetime.now())
+
+class Question(db.Model):
+    __tablename__ = "QuestionList"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    text = db.Column(db.String(300), nullable=False)
+
+    entry_time = db.Column(db.DateTime(), nullable=False, default=datetime.now())
