e72278f3c9a8bd86fa05169b923ce6caa8849786
Tyler Sengia <tylersengia@gmail.com>
2022-01-02 05:13:47 +0000

    Loading config

--- qabot/__init__.py
+++ qabot/__init__.py
@@ -2,4 +2,12 @@ import logging
 import pathlib
 import sqlalchemy
 
+from config import QABotTestConfig
+
 print("QA Bot Starting")
+print("Loading test config")
+
+conf = QABotTestConfig()
+
+db = sqlalchemy.create_engine(conf.DATABASEURI, echo=conf.SQLALCHEMY_ECHO)
+
